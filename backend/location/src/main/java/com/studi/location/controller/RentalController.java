5998d928573c77e3766d29e89a84089eb0c98c2d
ColeeneP <coleene.poudevigne@live.fr>
2023-01-29 16:11:47 +0100

    create Rental controller and service

---
+++ backend/location/src/main/java/com/studi/location/controller/RentalController.java
@@ -0,0 +1,89 @@
+package com.studi.location.controller;
+
+import com.studi.location.models.Rental;
+import com.studi.location.service.RentalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class RentalController {
+
+    @Autowired
+    private RentalService rentalService;
+
+    /**
+     * Create - Add a new rental
+     * @param rental An object rental
+     * @return The rental object saved
+     */
+    @PostMapping("/rental")
+    public Rental createRental(@RequestBody Rental rental) {
+        return rentalService.saveRental(rental);
+    }
+
+    /**
+     * Read - Get one rental
+     * @param id The id of the rental
+     * @return A Rental object fulfilled
+     */
+    @GetMapping("/rental/{id}")
+    public Rental getRental(@PathVariable("id") final Long id) {
+        Optional<Rental> rental = rentalService.getRental(id);
+        if(rental.isPresent()) {
+            return rental.get();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Read - Get all rentals
+     * @return - An Iterable object of Rentals fulfilled
+     */
+    @GetMapping("/rentals")
+    public Iterable<Rental> getRentals() {
+        return rentalService.getRentals();
+    }
+
+    /**
+     * Update - Update an existing rental
+     * @param id - The id of the rental to update
+     * @param rental - The rental object updated
+     * @return
+     */
+    @PutMapping("/rental/{id}")
+    public Rental updateRental(@PathVariable("id") final Long id, @RequestBody Rental rental) {
+        Optional<Rental> e = rentalService.getRental(id);
+        if(e.isPresent()) {
+            Rental currentRental = e.get();
+
+            Long flat = rental.getFlat();
+            if(flat != null) {
+                currentRental.setFlat(flat);
+            }
+            Long tenant = rental.getTenant();
+            if(tenant != null) {
+                currentRental.setTenant(tenant);
+            }
+            Boolean deposit = rental.getDeposit();
+            if(deposit != null) {
+                currentRental.setDeposit(deposit);
+            }
+            rentalService.saveRental(currentRental);
+            return currentRental;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Delete - Delete a rental
+     * @param id - The id of the rental to delete
+     */
+    @DeleteMapping("/rental/{id}")
+    public void deleteRental(@PathVariable("id") final Long id) {
+        rentalService.deleteRental(id);
+    }
+}
---
+++ backend/location/src/main/java/com/studi/location/service/RentalService.java
@@ -0,0 +1,35 @@
+package com.studi.location.service;
+
+import com.studi.location.models.Flat;
+import com.studi.location.models.Rental;
+import com.studi.location.repository.RentalRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Data
+@Service
+public class RentalService {
+
+    @Autowired
+    private RentalRepository rentalRepository;
+
+    public Optional<Rental> getRental(final Long id) {
+        return rentalRepository.findById(id);
+    }
+
+    public Iterable<Rental> getRentals() {
+        return rentalRepository.findAll();
+    }
+
+    public void deleteRental(final Long id) {
+        rentalRepository.deleteById(id);
+    }
+
+    public Rental saveRental(Rental rental) {
+        Rental savedRental = rentalRepository.save(rental);
+        return savedRental;
+    }
+}
