9e250b0a281d6d42fa475f47af6b77bd5bd73d21
ColeeneP <coleene.poudevigne@live.fr>
2023-01-29 16:38:28 +0100

    create Inventory controller and service

---
+++ backend/location/src/main/java/com/studi/location/controller/InventoryController.java
@@ -0,0 +1,95 @@
+package com.studi.location.controller;
+
+import com.studi.location.models.Flat;
+import com.studi.location.models.Inventory;
+import com.studi.location.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.Optional;
+
+@RestController
+public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    /**
+     * Create - Add a new inventory
+     * @param inventory An object inventory
+     * @return The inventory object saved
+     */
+    @PostMapping("/inventory")
+    public Inventory createInventory(@RequestBody Inventory inventory) {
+        return inventoryService.saveInventory(inventory);
+    }
+
+    /**
+     * Read - Get one inventory
+     * @param id The id of the inventory
+     * @return An Inventory object fulfilled
+     */
+    @GetMapping("/inventory/{id}")
+    public Inventory getInventory(@PathVariable("id") final Long id) {
+        Optional<Inventory> inventory = inventoryService.getInventory(id);
+        if(inventory.isPresent()) {
+            return inventory.get();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Read - Get all inventories
+     * @return - An Iterable object of Inventories fulfilled
+     */
+    @GetMapping("/inventories")
+    public Iterable<Inventory> getInventories() {
+        return inventoryService.getInventories();
+    }
+
+    /**
+     * Update - Update an existing inventory
+     * @param id - The id of the inventory to update
+     * @param inventory - The inventory object updated
+     * @return
+     */
+    @PutMapping("/inventory/{id}")
+    public Inventory updateInventory(@PathVariable("id") final Long id, @RequestBody Inventory inventory) {
+        Optional<Inventory> e = inventoryService.getInventory(id);
+        if(e.isPresent()) {
+            Inventory currentInventory = e.get();
+
+            Long rental = inventory.getRental();
+            if(rental != null) {
+                currentInventory.setRental(rental);
+            }
+            Enum status = inventory.getStatus();
+            if(status != null) {
+                currentInventory.setStatus(status);
+            }
+            Date date = inventory.getDate();
+            if(date != null) {
+                currentInventory.setDate(date);
+            }
+            String notes = inventory.getNotes();
+            if(notes != null) {
+                currentInventory.setNotes(notes);
+            }
+            inventoryService.saveInventory(currentInventory);
+            return currentInventory;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Delete - Delete an inventory
+     * @param id - The id of the inventory to delete
+     */
+    @DeleteMapping("/inventory/{id}")
+    public void deleteInventory(@PathVariable("id") final Long id) {
+        inventoryService.deleteInventory(id);
+    }
+}
---
+++ backend/location/src/main/java/com/studi/location/service/InventoryService.java
@@ -0,0 +1,34 @@
+package com.studi.location.service;
+
+import com.studi.location.models.Inventory;
+import com.studi.location.repository.InventoryRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Data
+@Service
+public class InventoryService {
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    public Optional<Inventory> getInventory(final Long id) {
+        return inventoryRepository.findById(id);
+    }
+
+    public Iterable<Inventory> getInventories() {
+        return inventoryRepository.findAll();
+    }
+
+    public void deleteInventory(final Long id) {
+        inventoryRepository.deleteById(id);
+    }
+
+    public Inventory saveInventory(Inventory inventory) {
+        Inventory savedInventory = inventoryRepository.save(inventory);
+        return savedInventory;
+    }
+}
