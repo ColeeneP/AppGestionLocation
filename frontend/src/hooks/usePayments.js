927f3fb705b7be53336a914ae53ca07e3776adeb
ColeeneP <coleene.poudevigne@live.fr>
2023-04-28 18:46:51 +0200

    modify OneTenant page

--- frontend/src/hooks/usePayments.js
+++ frontend/src/hooks/usePayments.js
@@ -3,7 +3,10 @@ import { GetPayments } from "../services/Payments";
 
 export const usePayments = (events) => {
     const [payments, setPayments] = useState([
-        {"id":1,"rental":{"id":1,"property":{"id":1,"address":"","additional":null,"postalCode":86370,"city":"","rent":480.0,"charges":20.0,"deposit":500.0,"available":true},"tenant":{"id":1,"firstname":"","lastname":"","birthday":"","phone":"","email":""},"deposit":true},"date":"","origin":"","amount":""}
+        {"id":1,
+            "rental":{"id":1,"property":{"id":1,"address":"","additional":null,"postalCode":86370,"city":"","rent":480.0,"charges":20.0,"deposit":500.0,"available":true},
+            "tenant":{"id":1,"firstname":"","lastname":"","birthday":"","phone":"","email":""},"deposit":true},
+        "date":"","origin":"","amount":""}
     ]);
     useEffect(() => {
         GetPayments().then((res) => setPayments(res.data)) .catch(error => alert(error.message))
--- frontend/src/pages/OneTenant.js
+++ frontend/src/pages/OneTenant.js
@@ -10,15 +10,26 @@ import EditIcon from '@mui/icons-material/Edit';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import Header from "../components/Header";
 import PersonIcon from '@mui/icons-material/Person';
+import { GetPaymentForTenant } from "../services/Payments";
+import { GetRentalsForTenant } from "../services/Rental";
 
 export default function OneTenant() {
     let {id} = useParams();
     const [tenant, setTenant] = useState([{"id": 1, "firstname": "", "lastname": "", "birthday": "1990-01-01", "phone": "", "email": ""}]);
     console.log(tenant);
+    const [payments, setPayments] = useState([
+        {"id":1,"rental":{"id":1,"property":{"id":1,"address":"1181 chemin du plan","additional":"BAT C APP 004","postalCode":86370,"city":"VIVONNE","rent":480.0,"charges":20.0,"deposit":500.0,"available":true},"tenant":{"id":1,"firstname":"John","lastname":"Smith","birthday":"1990-01-01","phone":"0601020304","email":"john.smith@email.fr"},"deposit":true},"date":"2023-04-28","origin":"Locataire","amount":"350"}
+    ]);
+    console.log(payments);
+
+    const [properties, setProperties] = useState([{"id":1,"property":{"id":1,"address":"1181 chemin du plan","additional":"BAT C APP 004","postalCode":86370,"city":"VIVONNE","rent":480.0,"charges":20.0,"deposit":500.0,"available":true},"tenant":{"id":1,"firstname":"John","lastname":"Smith","birthday":"1990-01-01","phone":"0601020304","email":"john.smith@email.fr"},"deposit":true}])
 
     useEffect(() => {
-        GetTenant(id).then(res => {setTenant(res.data);}).catch(error => error)
-    }, [])
+        GetTenant(id).then(res => {setTenant(res.data);}).catch(error => error);
+        GetPaymentForTenant(id).then(res => {setPayments(res.data); console.log(res.data);}).catch(error => error);
+        GetRentalsForTenant(id).then(res => {setProperties(res.data); console.log(res.data);}).catch(error => error);
+        
+    }, []);
 
     return (
         <div>
@@ -32,20 +43,25 @@ export default function OneTenant() {
             <div class="detail_info"><div>prénom</div><div>{tenant.firstname}</div><br /></div>
             <div class="detail_info"><div>date de naissance</div><div>{tenant.birthday}</div> </div><br />
             <div class="detail_info"><div>téléphone</div><div>{tenant.phone}</div><br /></div>
-            <div class="detail_info"><div>email</div><div>{tenant.email}</div></div>
-                
-                
-                
-                
-                
-                           
+            <div class="detail_info"><div>email</div><div>{tenant.email}</div></div>                 
             
         </div>
         <div class="tenant_property">
             <h2>Biens loués</h2>
+            {properties.map((property) => {
+                return(
+                    <div class="detail_property">{property.property.address}<br /> {property.property.additional}<br /> {property.property.postalCode} {property.property.city}</div>   
+                )
+            })}
+
         </div>
         <div class="tenant_payments">
             <h2>Solde</h2>
+            {payments.map((payment) => {
+                return(
+                    <div class="detail_payment"><div>{payment.amount}€ {payment.origin}</div><div>{payment.date}</div></div>   
+                )
+            })}
         </div>
 
         <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
