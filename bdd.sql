c4fc7ddf71ccd9ca5e535c2434aba5b2ebe71b8e
ColeeneP <coleene.poudevigne@live.fr>
2023-05-02 22:37:31 +0200

    change INT PK to SERIAL

--- bdd.sql
+++ bdd.sql
@@ -2,7 +2,7 @@ CREATE DATABASE immovable;
 \c immovable;
 
 CREATE TABLE IF NOT EXISTS tenant (
-    id INT NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     birthday DATE NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS tenant (
 );
 
 CREATE TABLE IF NOT EXISTS property (
-    id INT NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     address VARCHAR NOT NULL,
     additonal VARCHAR,
     postal_code CHAR(5) NOT NULL,
@@ -23,17 +23,17 @@ CREATE TABLE IF NOT EXISTS property (
 );
 
 CREATE TABLE IF NOT EXISTS rental (
-    id INT NOT NULL PRIMARY KEY,
-    property INT NOT NULL,
+    id SERIAL NOT NULL PRIMARY KEY,
+    property SERIAL NOT NULL ON DELETE CASCADE,
     FOREIGN KEY (property) REFERENCES property(id),
-    tenant INT NOT NULL,
+    tenant SERIAL NOT NULL ON DELETE CASCADE,
     FOREIGN KEY (tenant) REFERENCES tenant(id),
     deposit BOOLEAN DEFAULT false
 );
 
 CREATE TABLE IF NOT EXISTS payment (
-    id INT NOT NULL PRIMARY KEY,
-    rental INT NOT NULL,
+    id SERIAL NOT NULL PRIMARY KEY,
+    rental SERIAL NOT NULL ON DELETE CASCADE,
     FOREIGN KEY (rental) REFERENCES rental(id),
     date DATE DEFAULT CURRENT_TIMESTAMP,
     origin VARCHAR,
@@ -43,8 +43,8 @@ CREATE TABLE IF NOT EXISTS payment (
 CREATE TYPE status AS ENUM ('ingoing', 'outgoing');
 
 CREATE TABLE IF NOT EXISTS inventory (
-    id INT NOT NULL PRIMARY KEY,
-    property INT NOT NULL,
+    id SERIAL NOT NULL PRIMARY KEY,
+    property SERIAL NOT NULL ON DELETE CASCADE,
     FOREIGN KEY (property) REFERENCES property(id),
     status status,
     date DATE,
